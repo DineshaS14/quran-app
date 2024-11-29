de42316ee22141cb657e07037791f57335730702
Dinesha Shair <dms60488@uga.edu>
2024-11-29 06:39:52 -0500

    Google Analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,20 +4,11 @@ import "./globals.css";
 import NavBar from "../components/NavBar";
 import Footer from "@/components/Footer";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
 
 export const metadata: Metadata = {
   title: "Our Quran-Hub",
-  description: "Created By Dinesha",
+  description: "Created By DineshaS14",
 };
 
 export default function RootLayout({
@@ -27,8 +18,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      {/* Google Analytics Tag */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-3HWREM4M5Q"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-3HWREM4M5Q');
+            `,
+          }}
+        />
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased overvlow-y-auto max-h-screen overflow-x-hidden`}
+        className={` antialiased overvlow-y-auto max-h-screen overflow-x-hidden`}
       >
         <NavBar/>
         {children}
